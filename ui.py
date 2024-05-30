59b0a1dcc00cc1234765f1fd9335537d8fc1a5ef
blackbrokkoli <kastomat@gmail.com>
2024-05-30 19:38:07 +0200

    make progressbar work

--- ui.py
+++ ui.py
@@ -24,7 +24,7 @@ class Purger:
 
         self.progressbar = ttk.Progressbar(maximum=2000, mode="determinate")
         self.progressbar.pack(fill="x", padx=10, pady=10)
-        self.time_left = 2000
+        self.time_left = 0
         self.progressbar["value"] = self.time_left
         self.current_countdown = None
 
@@ -34,25 +34,23 @@ class Purger:
         self.img_label = tk.Label(root)
         self.img_label.place(relx=0.5, rely=0.5, anchor="center")
 
-        self.load_file()
-
         self.btn_1 = tk.Button(root, text="← Differenz", command=lambda: self.answer(1), bg="#333333", fg="#ffffff", font=("Arial", 24))
         self.btn_1.pack(padx=10, pady=10, side=tk.LEFT, fill=tk.X)
 
         self.btn_2 = tk.Button(root, text="Division →", command=lambda: self.answer(2), bg="#333333", fg="#ffffff", font=("Arial", 24))
         self.btn_2.pack(padx=10, pady=10, side=tk.RIGHT, fill=tk.X)
 
-
-
         # Bind arrow keys to button actions
         root.bind("<Left>", lambda event: self.answer(1))
         root.bind("<Right>", lambda event: self.answer(2))
 
-    def answer(self, answer):
         self.load_file()
+        self.update_progressbar()
+
+    def answer(self, answer):
+        self.reset_progressbar()
 
     def load_file(self):
-        self.cancel_countdown()
         decide_from_which_folder = random.randint(0, 1) + 1
         if decide_from_which_folder == 1:
             relevant_img_list = self.image_list_1
@@ -69,24 +67,20 @@ class Purger:
             self.img_label.configure(image=img_tk)
             self.img_label.image = img_tk
 
-        self.update_progressbar()
-
 
  
     def update_progressbar(self):
-        print(self.time_left)
         self.progressbar["value"] = self.time_left
-        self.time_left -= 100
-        if self.time_left < 0:
-            self.load_file()
-            self.time_left = 2000
-        else:
-            self.current_countdown = self.root.after(100, self.update_progressbar)
+        self.time_left += 10
+        self.current_countdown = self.root.after(10, self.update_progressbar)
+        if self.time_left >= 2000:
+            self.reset_progressbar()
 
-    def cancel_countdown(self):
-        if self.current_countdown:
-            self.root.after_cancel(self.current_countdown)
-            self.current_countdown = None
+    def reset_progressbar(self):
+        self.root.after_cancel(self.current_countdown)
+        self.time_left = 0
+        self.load_file()
+        self.update_progressbar()
 
 if __name__ == "__main__":
     root = tk.Tk()
